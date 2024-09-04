c851db6c3ae83a6a5976306abbdde645fc0d331a
mattae <matthew.edor@gmail.com>
2024-09-04 10:09:04 +0100

    Updates

---
+++ ImpactConnect/src/SetupStateProvider.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { SharedStateContext, useSharedState } from './utils.js'
+
+const SetupStateProvider = ({children}) => {
+  const sharedState = useSharedState()
+
+  return (
+      <SharedStateContext.Provider value={sharedState}>
+        {children}
+      </SharedStateContext.Provider>
+  )
+}
+
+SetupStateProvider.propTypes = {
+  children: PropTypes.node,
+}
+
+export default SetupStateProvider
---
+++ ImpactConnect/src/components/OrganisationUnitComponent.js
@@ -0,0 +1,41 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import { OrganisationUnitTree } from '@dhis2-ui/organisation-unit-tree'
+import React from 'react'
+
+const query = {
+    results:{
+        resource: 'organisationUnits',
+        params: {
+            fields: "id,name,level,displayShortName",
+            filter: 'level:eq:1'
+        }
+    }
+}
+
+
+const OrganisationUnitComponent = ({ handleOUChange, selectedOU }) => {
+    const { loading, error, data } = useDataQuery(query)
+
+    if (error) {
+        return <span>ERROR: {error.message}</span>
+    }
+
+    if (loading) {
+        return <span>Loading...</span>
+    }
+
+    return (
+        <div>
+            <div>
+                <OrganisationUnitTree
+                    name={data['results']['organisationUnits'][0]['name']}
+                    onChange={handleOUChange}
+                    roots={[data['results']['organisationUnits'][0]['id']]}
+                    selected={selectedOU}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default OrganisationUnitComponent
