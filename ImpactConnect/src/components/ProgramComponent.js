54a0fc3c9fca1c7085fabead0167c642b30dc0ea
eabumere <abumere.ejakhegbe@gmail.com>
2024-09-12 11:28:48 +0100

    - Implemented data values updated

--- ImpactConnect/src/components/ProgramComponent.js
+++ ImpactConnect/src/components/ProgramComponent.js
@@ -1,7 +1,6 @@
 import { useDataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n';
-import React, { useContext } from 'react'
-import { SharedStateContext } from '../utils';
+import React from 'react'
 
 /*  Query Parameters**/
 const query = {
